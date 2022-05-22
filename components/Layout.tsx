ffba3ee03f26882a8adb130efbbf0397551e363a
addonion <addonion@gmail.com>
2022-05-22 19:18:28 +0500

    Update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,16 +15,17 @@ const Layout = ({ children }: Props) => {
         <link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="/fonts/MabryProBold.woff2" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="yandex-verification" content="d0559dbd0d368d3d" />
-        {/* Google Tag Manager */}
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      </Head>
+
+      {/* Google Tag Manager */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-K7H5S7N');`}
-        </Script>
-        {/* End Google Tag Manager */}
-      </Head>
+      </Script>
+      {/* End Google Tag Manager */}
       {/* Google Tag Manager (noscript) */}
       <noscript>
         <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7H5S7N" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
