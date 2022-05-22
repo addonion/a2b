020ce7e6fec773039ffe136f5b1c5bfe096302a3
addonion <addonion@gmail.com>
2022-05-22 11:35:10 +0500

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,24 +19,39 @@ const Home: NextPage = () => {
       <Layout>
         <section className={styles.hero}>
           <div className='container mx-auto flex flex-col h-full px-4 py-5 md:py-9'>
-            <div className='my-auto md:mb-8 landscape:hidden lg:landscape:block'>
-              <Image src={heroPic} width={1504} height={657} layout="responsive" alt="Art2Buisness" />
+            <div className='my-auto md:mb-8'>
+              <div className='landscape:hidden md:landscape:block'>
+                <Image src={heroPic} width={1504} height={657} layout="responsive" priority={true} alt="Art2Buisness" />
+              </div>
+              <h1 className='w-full xl:w-5/6 mt-10'>
+                Мы создаем концепции, мероприятия и образовательные проекты, вдохновляясь современным искусством
+              </h1>
             </div>
-            <h1 className='w-full xl:w-5/6 mt-auto'>
-              Мы создаем концепции, мероприятия и образовательные проекты, вдохновляясь современным искусством
-            </h1>
+            
           </div>
         </section>
 
-        <section>
-          <div className='container mx-auto flex flex-col h-full px-4 py-5 md:py-9'>
-            <div className='my-auto md:mb-8 landscape:hidden lg:landscape:block'>
-              <Image src={aboutPic} width={1504} height={601} layout="responsive" alt="Art2Buisness" />
-            </div>
-            <h2>
-              ТИМБИЛДИНГ С СОВРЕМЕННЫМ ИСКУССТВОМ
-            </h2>
+        <nav className={styles.navigation}>
+          <div className='container mx-auto'>
+            {[
+              ['Тимбилдинг', '#teambuilding'],
+              ['Цена', '#price'],
+              ['А ещё мы…', '#more'],
+              ['Наша команда', '#team'],
+              ['Контакты', '#contacts']
+            ].map(([title, url]) => (
+              <a href={url} key={url}>{title}</a>
+            ))}
           </div>
+        </nav>
+
+        <section id='teambuilding' className='container h-screen mx-auto flex flex-col px-4 py-5 md:py-9'>
+            <div className='my-auto'>
+              <Image src={aboutPic} width={1504} height={601} layout="responsive" alt="Art2Buisness" />
+              <h2 className='mt-10'>
+                ТИМБИЛДИНГ С СОВРЕМЕННЫМ ИСКУССТВОМ
+              </h2>
+            </div>
         </section>
       </Layout>
     </>
