ae9b983a37ab18988412f790676c3b2e316141ae
addonion <addonion@gmail.com>
2022-05-22 20:21:56 +0500

    Update

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,23 +14,22 @@ const Layout = ({ children }: Props) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="/fonts/MabryProBold.woff2" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="yandex-verification" content="d0559dbd0d368d3d" />
       </Head>
 
-      {/* Google Tag Manager */}
+      {/* Yandex.Metrika counter */}
       <Script id="google-analytics" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K7H5S7N');`}
+        {`
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        ym(88858866, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+        });
+        `}
       </Script>
-      {/* End Google Tag Manager */}
-      {/* Google Tag Manager (noscript) */}
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7H5S7N" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
-      </noscript>
-      {/* End Google Tag Manager (noscript) */}
+      {/* /Yandex.Metrika counter */}
 
       {children}
     </>
