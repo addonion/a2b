c380dce0e2a612689a74e56307e4a35c70622243
addonion <addonion@gmail.com>
2022-05-22 12:22:12 +0500

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,17 @@ import Image from 'next/image'
 import styles from '../styles/Main.module.scss'
 
 // Images
-import heroPic from '../images/1.png'
-import aboutPic from '../images/2.png'
+import heroPic from '../images/hero.png'
+import teambuildingPic from '../images/teambuilding.png'
+import contactsPic from '../images/contacts.png'
+
+const menuItems = [
+  ['Тимбилдинг', '#teambuilding'],
+  ['Цена', '#price'],
+  ['А ещё мы…', '#more'],
+  ['Наша команда', '#team'],
+  ['Контакты', '#contacts']
+]
 
 const Home: NextPage = () => {
   return (
@@ -17,41 +26,48 @@ const Home: NextPage = () => {
       </Head>
 
       <Layout>
-        <section className={styles.hero}>
-          <div className='container mx-auto flex flex-col h-full px-4 py-5 md:py-9'>
-            <div className='my-auto md:mb-8'>
-              <div className='landscape:hidden md:landscape:block'>
+        <section className={`${styles.hero} py-64 xl:py-16`}>
+          <div className='container mx-auto flex flex-col h-full px-4'>
+            <div className='my-auto md:mb-0'>
+              <div className='hidden md:block'>
                 <Image src={heroPic} width={1504} height={657} layout="responsive" priority={true} alt="Art2Buisness" />
               </div>
-              <h1 className='w-full xl:w-5/6 mt-10'>
+              <h1 className='w-full xl:w-5/6 md:mt-10 xl:mt-16'>
                 Мы создаем концепции, мероприятия и образовательные проекты, вдохновляясь современным искусством
               </h1>
             </div>
-            
           </div>
         </section>
 
-        <nav className={styles.navigation}>
-          <div className='container mx-auto'>
-            {[
-              ['Тимбилдинг', '#teambuilding'],
-              ['Цена', '#price'],
-              ['А ещё мы…', '#more'],
-              ['Наша команда', '#team'],
-              ['Контакты', '#contacts']
-            ].map(([title, url]) => (
-              <a href={url} key={url}>{title}</a>
+        <nav className={`${styles.navigation} hidden lg:block`}>
+          <div className='container mx-auto flex'>
+            {menuItems.map(([title, url]) => (
+              <a href={url} key={url} className={`${styles.navigation__link} pl-4 pr-10 py-4`}>{title}</a>
             ))}
           </div>
         </nav>
 
-        <section id='teambuilding' className='container h-screen mx-auto flex flex-col px-4 py-5 md:py-9'>
-            <div className='my-auto'>
-              <Image src={aboutPic} width={1504} height={601} layout="responsive" alt="Art2Buisness" />
-              <h2 className='mt-10'>
-                ТИМБИЛДИНГ С СОВРЕМЕННЫМ ИСКУССТВОМ
-              </h2>
-            </div>
+        <section id='teambuilding' className={`${styles.teambuilding} container mx-auto flex flex-col px-4 py-16 md:py-64`}>
+          <Image src={teambuildingPic} width={1504} height={601} layout="responsive" alt="Art2Buisness" />
+          <h2 className='mt-10'>
+            ТИМБИЛДИНГ С СОВРЕМЕННЫМ ИСКУССТВОМ
+          </h2>
+        </section>
+
+        <section id='contacts' className={`${styles.teambuilding} container mx-auto grid grid-cols-2 gap-4 px-4`}>
+          <div className='my-auto'>
+            <h2 className='mb-2'>КОНТАКТЫ:</h2>
+            <p>
+              +7 (919) 488-66-76 — Анастасия,
+              <br />
+              +7 (951) 942-82-85 — Лейла,
+              <br />
+              art2.business@yandex.ru
+            </p>
+          </div>
+          <div>
+            <Image src={contactsPic} width={744} height={1062} layout="responsive" alt="Art2Buisness" />
+          </div>
         </section>
       </Layout>
     </>
