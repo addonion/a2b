a6c1c020b4998e9efc2834411ee0f0cc90aa84c6
addonion <addonion@gmail.com>
2022-05-29 18:30:55 +0500

    Favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,11 +13,24 @@ const Layout = ({ children }: Props) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="preload" as="font" type="font/woff2" crossOrigin="anonymous" href="/fonts/MabryProBold.woff2" />
-        <link rel="icon" href="/favicon.ico" />
+
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff90e8" />
+        <meta name="apple-mobile-web-app-title" content="Art2Business" />
+        <meta name="application-name" content="Art2Business" />
+        <meta name="msapplication-TileColor" content="#9f00a7" />
+        <meta name="theme-color" content="#ffffff" />
+        {/*  /Favicon */}
       </Head>
 
+      {children}
+
       {/* Yandex.Metrika counter */}
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="yandexMetrika" strategy="afterInteractive">
         {`
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -30,8 +43,6 @@ const Layout = ({ children }: Props) => {
         `}
       </Script>
       {/* /Yandex.Metrika counter */}
-
-      {children}
     </>
   );
 };
