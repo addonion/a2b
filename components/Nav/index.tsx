8e14c2f9f292dca787513adb35b2928a998d6c83
addonion <addonion@gmail.com>
2022-11-21 18:01:27 +0600

    Fix

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -38,7 +38,7 @@ const Nav = (): JSX.Element => {
           ["Наша команда", "/#team"],
           ["Контакты", "/#contacts"],
         ].map(([title, url]) => (
-          <Link href={url} key={url} className={`${styles.navigation__link} ${!isOpen ? "hidden" : "block"} md:inline-block pr-8 lg:pr-12 py-2 last:pr-0`}>
+          <Link href={url} onClick={() => setOpen(false)} key={url} className={`${styles.navigation__link} ${!isOpen ? "hidden" : "block"} md:inline-block pr-8 lg:pr-12 py-2 last:pr-0`}>
             {title}
           </Link>
         ))}
