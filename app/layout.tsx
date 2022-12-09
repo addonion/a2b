5c95567242f14e96b3ae196e407954b474c17ec7
addonion <addonion@gmail.com>
2022-12-09 16:10:30 +0600

    Upgrade

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <body>{children}</body>
+
+      {/* Google Analitics 4 */}
       <Script src="https://tag.art2business.ru/gtag/js?id=G-P1V8VWHRHN" strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
