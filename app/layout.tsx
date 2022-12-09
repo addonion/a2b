3ef1ef89b1c87a625275d03ae5e05442b5611ce2
addonion <addonion@gmail.com>
2022-12-09 13:24:26 +0600

    GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,20 @@
+import Script from "next/script";
 import "../styles/globals.scss";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
       <body>{children}</body>
+      <Script src="https://tag.art2business.ru/gtag/js?id=G-P1V8VWHRHN" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-P1V8VWHRHN');
+        `}
+      </Script>
     </html>
   );
 }
