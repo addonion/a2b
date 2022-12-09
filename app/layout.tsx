008f4a9d4b997c9cd14c92d866003c76623af747
addonion <addonion@gmail.com>
2022-12-09 14:28:14 +0600

    GA4 final

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ru">
       <body>{children}</body>
-      <Script id="gtm" strategy="afterInteractive">
-        {`
-          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://tag.art2business.ru/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NDHMFZ5'
-        `}
-      </Script>
       <Script src="https://tag.art2business.ru/gtag/js?id=G-P1V8VWHRHN" strategy="afterInteractive" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
